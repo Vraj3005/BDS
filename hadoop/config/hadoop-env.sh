c998759dbbda155d88dbba913db1cd6f8669ed8d
yashkacha26 <yaskacha977@gmail.com>
2026-09-25 12:36:39 +0530

    Configured Hadoop HDFS cluster and environment templates

---
+++ hadoop/config/hadoop-env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Hadoop environment template
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
+export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
